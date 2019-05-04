808b48cf67e8d0254dcff08ba168b9dcecc87a89
porcuquine <porcuquine@users.noreply.github.com>
2019-05-03 18:47:56 -0700

    Refactor/fix/document plan.

--- orient.lisp
+++ orient.lisp
@@ -19,8 +19,17 @@
   ((inputs :initarg :inputs :initform '() :accessor signature-inputs)
    (outputs :initarg :outputs :initform '() :accessor signature-outputs)))
 
+
 (defun make-signature (inputs outputs)
   (make-instance 'signature :inputs inputs :outputs outputs))
+
+(defun pruned-signature (sig)
+  "Return a new signature, with outputs which are also inputs pruned, since these will be trivially provided."
+  (let* ((inputs (signature-inputs sig))
+	 (pruned-outputs (set-difference (signature-outputs sig) inputs)))
+    (if pruned-outputs
+	(make-signature inputs pruned-outputs)
+	sig)))
 
 (defmethod print-object ((sig signature) (stream t))
   (format stream "(SIG ~A -> ~A)" (signature-inputs sig) (signature-outputs sig)))
@@ -44,7 +53,6 @@
 (defmethod print-object ((trans transform) (stream t))
   (format stream "(TRANSFORM ~A === ~A)" (transform-signature trans) (transform-implementation trans)))
 
-
 (defun identity-transform () (make-instance 'transform))
 
 (defclass component ()
@@ -66,30 +74,30 @@
 
 (defclass engine () ())
 
-(defun plan-step (signature-to-apply transform)
-  (cons signature-to-apply transform))
-
 (defvar *transforms-tried* 0)
 
 (defgeneric %plan (element signature system plan)
   (:method ((transform transform) (signature signature) (system system) (plan list))
     (incf *transforms-tried*)
     (let* ((sig (transform-signature transform))
-	   (provided-outputs (provides (signature-outputs signature) sig)))
-
+	   ;; Which of the still-needed outputs, if any, does the this transform's signature provide?
+	   (provided-outputs (provides (signature-outputs signature) sig)))     
       (unless provided-outputs
-	(return-from %plan '()))
-     
-      (let ((new-plan (cons transform plan))
-	    (remaining-outputs (set-difference (signature-outputs sig) provided-outputs)))       
-	(when remaining-outputs
-	  (let ((diminished-signature (make-signature (signature-inputs signature) remaining-outputs)))
-	    (return-from %plan (%plan :system diminished-signature system new-plan))))
-	(let ((remaining-inputs (set-difference (signature-inputs sig) (signature-inputs signature))))
-	  (if remaining-inputs
-	    (let ((new-sig (make-signature (signature-inputs signature) remaining-inputs)))
-	      (%plan :system new-sig system new-plan))
-	    new-plan)))))
+	;; If this transform doesn't provide any needed outputs, fail early.
+	(return-from %plan nil))
+
+      ;; Otherwise, add the transform to the plan and update the signature to satisfy.
+      (let* ((new-plan (cons transform plan))
+	     ;; Inputs of the current transform which aren't trivially provided must now be outputs of the remaining plan (to be provided before
+	     ;; this step's transform is applied).
+	     (additional-outputs (set-difference (signature-inputs sig) (signature-inputs signature)))
+	     ;; Outputs which still need to be provided.
+	     (remaining-outputs (union (set-difference (signature-outputs sig) provided-outputs) additional-outputs)))
+	(if remaining-outputs
+	    ;; If there are still outputs which need to be satisfied, continue planning the system.
+	    (%plan :system (make-signature (signature-inputs signature) remaining-outputs) system new-plan)
+	    ;; Otherwise, return the new plan.
+	    new-plan))))
   (:method ((component component) (signature signature) (system system) (plan list))
     (mapcan (lambda (transform)
 	      (%plan transform signature system plan))
@@ -101,7 +109,7 @@
 
 (defun plan (signature system)
   (let ((*transforms-tried* 0))
-    (values (%plan :system signature system '())
+    (values (%plan :system (pruned-signature signature) system '())
 	    *transforms-tried*)))
 
 ;;; Syntax
@@ -127,9 +135,10 @@
 (defparameter sig3 (sig (a b c) -> (e f)))
 
 (defparameter s1 (sys ((component ((transform (a b c) -> (d) === :asdf))))))
-(defparameter s2 (sys ((component ((transform (a b c) -> (d) === :asdf)))
-		       (component ((transform (x y z) -> (q) === :uiop)))
-		       (component ((transform (b c d) -> (e f) === :fdsa))))))
+
+(defparameter s2 (sys ((component ((transform (a b c) -> (d) === :asdf)
+				   (transform (x y z) -> (q) === :uiop)
+				   (transform (b c d) -> (e f) === :fdsa))))))
 
 #|
 (plan sig1 s1) => (((SIG (A B C) -> (D)) . (TRANSFORM (SIG (A B C) -> (D)) === ASDF)))
@@ -140,6 +149,6 @@
 
 (plan sig2 s2) => (((TRANSFORM (SIG (B C D) -> (E F)) === FDSA))                         ; *transforms-tried* 3
 
-(plan sig2 s3) => ((TRANSFORM (SIG (A B C) -> (D)) === ASDF)
+(plan sig3 s2) => ((TRANSFORM (SIG (A B C) -> (D)) === ASDF)
 		   (TRANSFORM (SIG (B C D) -> (E F)) === FDSA))                          ; *transforms-tried* 6
 |#
