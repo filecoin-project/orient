8c5c65bd404864bb98f1e5d4f1dfa5f397b270fa
porcuquine <porcuquine@users.noreply.github.com>
2019-08-08 00:03:13 -0700

    Use /bin/sh so dot will be in path.

--- base/orient.lisp
+++ base/orient.lisp
@@ -699,13 +699,11 @@
   (with-output-to-string (out)
     (write-dot-format directed-graph out :base-url base-url)))
 
-(defvar *dot-path* "/usr/local/bin/dot")
-
 (defun dot (dot-format &key format output-file (layout "dot"))
   (with-input-from-string (in dot-format)
     ;; UIOP:RUN-PROGRAM seems to fail with error that file does not exist when we try this on Heroku. Not sure if a linux or Heroku thing or what.
     #+sbcl
-    (sb-ext:run-program *dot-path* (list (format nil "-K~A" layout) "-T" format) :output output-file :input in :if-output-exists :supersede)
+    (sb-ext:run-program "/bin/sh" (list "-c" (format nil "dot -K~A -T ~A" layout format)) :output output-file :input in :if-output-exists :supersede)
     #-sbcl
     (uiop:run-program (format nil "dot -K~A -T ~A" layout format) :output output-file :input in)))
 
