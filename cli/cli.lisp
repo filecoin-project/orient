a672eaae662d742b0ebbc633248f4a3da2a161c6
porcuquine <porcuquine@users.noreply.github.com>
2019-09-27 12:36:35 -0700

    Improve CLI exit codes.

--- cli/cli.lisp
+++ cli/cli.lisp
@@ -22,104 +22,115 @@
 	     ,@body)
 	   (let ((*out* *standard-output*)) ,@body)))))
 
+(defun format-error (format-string &rest format-args)
+  (apply #'format *error-output* format-string format-args)
+  (signal 'error))
+
 (defun main (&optional argv)
-  (with-cli-options ((cli-options) t)
-      (&parameters (in (in "FILE" "JSON input file, specify -- to use stdin"))
-		   (flags (flags "FLAGS" "provided flags select components so labeled in system."))
-		   (out (out "FILE" "JSON output file, otherwise stdout"))
-		   (system (system "FILE" "URI or filename of system to use"))
-		   (port (port "port-number" "port to listen on"))
-		   (merge (merge nil "merge inputs with (instead of replacing) defaults"))
-		   (command (command "{dump, graph, solve, report, test, web}" "<COMMAND>: may be provided as free token (without flag)."))
-		   (root (root "project root, so we can find json files"))
-		   &free commands)
-    (map-parsed-options (cli-options) nil '("in" "i"
-					    "flags" "f"
-					    "out" "o"
-					    "port" "p"
-					    "system" "s"
-					    "command" "c"
-					    "merge" "m"
-					    "root" "r") ;; Need to include all parameters from WITH-CLI-OPTIONS here.
-			(lambda (option value) (declare (ignore option value)))
-			(lambda (free-val) (declare (ignore free-val))))
-    (when root
-      (setq orient.base.util:*project-root* (truename root)))
-    
-    (destructuring-bind (&optional arg0 free-command &rest subcommands) commands
-      (declare (ignore arg0 subcommands))
-      (let ((command (keywordize (if command
-				     (progn (assert (not free-command))
-					    command)
-				     free-command))))
-	(when (eql command :test)
-	  (asdf:test-system :orient)
-	  (return-from main t))
-	(with-json-encoding ((find-package :orient.lang))
-	  (let* ((*schema-package* (find-package :orient.lang))
-		 (*package* (find-package :orient.lang))
-		 (json:*json-symbols-package* :orient.lang) ;; FIXME: remove need to expose use of JSON package here.
-		 (input (cond
-			  ((equal in "--") (get-json-relation-list *standard-input*))
-			  (in			   
-			   (get-json-relation-list in))))
-		 (flags (remove nil (mapcar #'interface:camel-case-to-lisp* (orient.base.util:string-split #\,  flags))))
-		 (raw-system (when system (get-system system)))
-		 (system (when raw-system (prune-system-for-flags raw-system flags))))
-	    (with-output (out)
-	      (case command
-		((:web)
-		 (let ((acceptor (if port
-				     (web:start-web :port (parse-integer port))
-				     (web:start-web))))
-		   (when acceptor
-		     (format *error-output* "Orient webserver started on port ~S" (hunchentoot:acceptor-port acceptor)))
-		   (handler-case (bt:join-thread (find-if (lambda (th)
-							    (search "hunchentoot" (bt:thread-name th)))
-							  (bt:all-threads)))
-		     ;; https://stackoverflow.com/questions/48103501/deploying-common-lisp-web-applications
-		     ;; Catch a user's C-c
-		     (#+sbcl sb-sys:interactive-interrupt
-		       #+ccl  ccl:interrupt-signal-condition
-		       #+clisp system::simple-interrupt-condition
-		       #+ecl ext:interactive-interrupt
-		       #+allegro excl:interrupt-signal
-		       () (progn
-			    (format *error-output* "Aborting.~&")
-			    (web:stop-web)
-			    (uiop:quit)))
-		     (error (c) (format t "Woops, an unknown error occured:~&~a~&" c)))
-		   (let ((*package* (find-package :orient.web)))
-		     
-		     (sb-impl::toplevel-repl nil))))
-		((:solve)
-		 (cond
-		   (system
-		    (let ((override-data (and merge input))
-			  (input (and (not merge) input)))
-		      (solve-system :system system :input input :override-data override-data)))
-		   (t (format *error-output* "No system specified.~%"))))
-		((:report)
-		 (cond
-		   (system
-		    (let ((override-data (and merge input))
-			  (input (and (not merge) input)))
-		      (report-system :system system :initial-data input :override-data override-data)))
-		   (t (format *error-output* "No system specified.~%"))))
-		((:graph)
-		 (cond
-		   (system
-		    (let ((override-data (and merge input))
-			  (input (and (not merge) input)))
-		      (graph-plan :system system :input input :override-data override-data)))
-		   (t (format *error-output* "No system specified.~%")))
-		 )
-		((:dump)
-		 (dump-json :system system *out* :expand-references t))
-		(otherwise
-		 ;; TODO: Generate this list and share with options doc.
-		 (format t "Usage: ~A command~%  command is one of {dump, graph, solve, report, test, web}~%" (car argv))))
-	      (terpri))))))))
+  (handler-case
+      (progn
+	(with-cli-options ((cli-options) t)
+	    (&parameters (in (in "FILE" "JSON input file, specify -- to use stdin"))
+			 (flags (flags "FLAGS" "provided flags select components so labeled in system."))
+			 (out (out "FILE" "JSON output file, otherwise stdout"))
+			 (system (system "FILE" "URI or filename of system to use"))
+			 (port (port "port-number" "port to listen on"))
+			 (merge (merge nil "merge inputs with (instead of replacing) defaults"))
+			 (command (command "{dump, graph, solve, report, test, web}" "<COMMAND>: may be provided as free token (without flag)."))
+			 (root (root "project root, so we can find json files"))
+			 &free commands)
+	  (map-parsed-options (cli-options) nil '("in" "i"
+						  "flags" "f"
+						  "out" "o"
+						  "port" "p"
+						  "system" "s"
+						  "command" "c"
+						  "merge" "m"
+						  "root" "r") ;; Need to include all parameters from WITH-CLI-OPTIONS here.
+			      (lambda (option value) (declare (ignore option value)))
+			      (lambda (free-val) (declare (ignore free-val))))
+	  (when root
+	    (setq orient.base.util:*project-root* (truename root)))
+	  
+	  (destructuring-bind (&optional arg0 free-command &rest subcommands) commands
+	    (declare (ignore arg0 subcommands))
+	    (let ((command (keywordize (if command
+					   (progn (assert (not free-command))
+						  command)
+					   free-command))))
+	      (when (eql command :test)
+		(asdf:test-system :orient)
+		(return-from main t))
+	      (with-json-encoding ((find-package :orient.lang))
+		(let* ((*schema-package* (find-package :orient.lang))
+		       (*package* (find-package :orient.lang))
+		       (json:*json-symbols-package* :orient.lang) ;; FIXME: remove need to expose use of JSON package here.
+		       (input (cond
+				((equal in "--") (get-json-relation-list *standard-input*))
+				(in			   
+				 (get-json-relation-list in))))
+		       (flags (remove nil (mapcar #'interface:camel-case-to-lisp* (orient.base.util:string-split #\,  flags))))
+		       (raw-system (when system (get-system system)))
+		       (system (when raw-system (prune-system-for-flags raw-system flags))))
+		  (with-output (out)
+		    (case command
+		      ((:web)
+		       (let ((acceptor (if port
+					   (web:start-web :port (parse-integer port))
+					   (web:start-web))))
+			 (when acceptor
+			   (format-error "Orient webserver started on port ~S" (hunchentoot:acceptor-port acceptor)))
+			 (handler-case (bt:join-thread (find-if (lambda (th)
+								  (search "hunchentoot" (bt:thread-name th)))
+								(bt:all-threads)))
+			   ;; https://stackoverflow.com/questions/48103501/deploying-common-lisp-web-applications
+			   ;; Catch a user's C-c
+			   (#+sbcl sb-sys:interactive-interrupt
+			     #+ccl  ccl:interrupt-signal-condition
+			     #+clisp system::simple-interrupt-condition
+			     #+ecl ext:interactive-interrupt
+			     #+allegro excl:interrupt-signal
+			     () (progn
+				  (format-error "Aborting.~&")
+				  (web:stop-web)
+				  (uiop:quit)))
+			   (error (c) (format-error "Woops, an unknown error occured:~&~a~&" c)))
+			 (let ((*package* (find-package :orient.web)))
+			   
+			   (sb-impl::toplevel-repl nil))))
+		      ((:solve)
+		       (cond
+			 (system
+			  (let ((override-data (and merge input))
+				(input (and (not merge) input)))
+			    (solve-system :system system :input input :override-data override-data)))
+			 (t (format-error "No system specified.~%"))))
+		      ((:report)
+		       (cond
+			 (system
+			  (let ((override-data (and merge input))
+				(input (and (not merge) input)))
+			    (report-system :system system :initial-data input :override-data override-data)))
+			 (t (format-error "No system specified.~%"))))
+		      ((:graph)
+		       (cond
+			 (system
+			  (let ((override-data (and merge input))
+				(input (and (not merge) input)))
+			    (graph-plan :system system :input input :override-data override-data)
+			    (sb-ext:exit :code 0)))
+			 (t (format-error "No system specified.~%"))))
+		      ((:dump)
+		       (dump-json :system system *out* :expand-references t))
+		      (otherwise
+		       ;; TODO: Generate this list and share with options doc.
+		       (format-error "Usage: ~A command~%  command is one of {dump, graph, solve, report, test, web}~%" (car argv))))
+		    (terpri)
+
+		    ))))))
+	(sb-ext:exit :code 0))
+    (error () (sb-ext:exit :code 1))
+    (condition () (sb-ext:exit :code 0))))
 
 (defun choose-system (spec)
   (case spec
